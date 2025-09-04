5529410c5273975d3876e1a06bd46f6163b19b15
manuelpiresluis <manuelpires@gmail.com>
2025-09-04 21:30:38 +0100

    refactor: uncomment navigation logic in Login component

--- front/motorSport/src/page/auth/login.tsx
+++ front/motorSport/src/page/auth/login.tsx
@@ -1,13 +1,13 @@
 import Style from "./index.module.css";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogin = (event: React.FormEvent) => {
-         event.preventDefault();
-        // return navigate("/dashboard");
-    }
+  const handleLogin = (event: React.FormEvent) => {
+    event.preventDefault();
+    return navigate("/dashboard");
+  };
   return (
     <div className={Style.loginContainer}>
       <div className={Style.background}></div>
@@ -21,10 +21,12 @@ const Login = () => {
           <br />
           <label>
             Password:
-            <input type="password"placeholder="Username" />
+            <input type="password" placeholder="Username" />
           </label>
           <br />
-          <button onClick={handleLogin} type="submit">Login</button>
+          <button onClick={handleLogin} type="submit">
+            Login
+          </button>
         </div>
       </form>
     </div>
